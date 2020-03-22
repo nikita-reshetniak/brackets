c866fc3fca70646d0cc168d4511a6a7d2d016281
Nikita <nikreshetnjak@gmail.com>
2020-03-22 15:45:00 +0100

    First commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const opens = bracketsConfig.map(x => x[0]);
+  const closes = bracketsConfig.map(x => x[1]);
+  
+  let equals = {};
+  bracketsConfig.forEach(element => {
+    if (element[0] == element[1]) {
+      equals[element[0]] = 0;
+    }
+  });
+
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (equals.hasOwnProperty(char)) {
+      equals[char] += i % 2 == 0 ?  1 : -1;
+    } else if (opens.includes(char)) {
+      stack.push(char);
+    } else if (opens.indexOf(stack[stack.length - 1]) == closes.indexOf(char)) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  
+  for (key in equals) {
+    if (equals[key] != 0) return false;
+  }
+
+  return stack.length == 0;
 }
